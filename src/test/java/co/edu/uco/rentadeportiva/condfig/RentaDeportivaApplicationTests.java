11e7932a980d8d4aa6b90a971aeb684966072051
Sebastian <sebassuarez103@gmail.com>
2024-04-18 23:53:26 -0500

    Initial commit

---
+++ src/test/java/co/edu/uco/rentadeportiva/condfig/RentaDeportivaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.rentadeportiva.condfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaDeportivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
