8b31675507032c0ce82c69ffd1ba9c836bda4f2d
Tamayoawo49 <davidtama1204@gmail.com>
2024-06-06 16:29:36 -0500

    Use case y fachada de la transaccion casi completados para actualizar tipo de espacio deportivo

---
+++ src/main/java/co/edu/uco/unidaddeportivaelbernabeu/business/fachade/concrete/espaciosdeportivos/ActualizarTipoEspacioDeportivoFachadaImpl.java
@@ -0,0 +1,49 @@
+package co.edu.uco.unidaddeportivaelbernabeu.business.fachade.concrete.espaciosdeportivos;
+
+import co.edu.uco.unidaddeportivaelbernabeu.business.assembler.dto.concrete.TipoEspacioDeportivoDTODomainAssembler;
+import co.edu.uco.unidaddeportivaelbernabeu.business.fachade.FacadeWithoutReturn;
+import co.edu.uco.unidaddeportivaelbernabeu.business.usecase.concrete.RegistrarTipoEspacioDeportivoImpl;
+import co.edu.uco.unidaddeportivaelbernabeu.crosscutting.exceptions.UnidadDeportivaElBernabeuException;
+import co.edu.uco.unidaddeportivaelbernabeu.crosscutting.exceptions.custom.BusinessUDElBernabeuException;
+import co.edu.uco.unidaddeportivaelbernabeu.data.dao.factory.DAOFactory;
+import co.edu.uco.unidaddeportivaelbernabeu.data.dao.factory.enums.Factory;
+import co.edu.uco.unidaddeportivaelbernabeu.dto.TipoEspacioDeportivoDTO;
+
+public class ActualizarTipoEspacioDeportivoFachadaImpl implements FacadeWithoutReturn<TipoEspacioDeportivoDTO> {
+
+    private DAOFactory factory;
+
+    public ActualizarTipoEspacioDeportivoFachadaImpl() {
+        factory = DAOFactory.getFactory(Factory.AZURE_SQL);
+    }
+
+
+    @Override
+    public void ejecutar(TipoEspacioDeportivoDTO tipoEspacioDeportivo) {
+        factory.iniciarTransaccion();
+
+        try {
+            var usecase = new RegistrarTipoEspacioDeportivoImpl(factory);
+            var tipoEspacioDeportivoDomain = TipoEspacioDeportivoDTODomainAssembler.obtenerInstancia().ensamblarDominio(tipoEspacioDeportivo);
+            usecase.ejecutar(tipoEspacioDeportivoDomain);
+
+            factory.confirmarTransaccion();
+        } catch (final UnidadDeportivaElBernabeuException exception) {
+            factory.cancelarTransaccion();
+            throw exception;
+        } catch (final Exception exception) {
+            factory.cancelarTransaccion();
+
+            var mensajeUsuario = "Se ha presentado un errror tratando de editar el tipo de espacio deportivo";
+            var mensajeTecnico = "Se ha presentado un problema inesperado tratando de editar la informacion del tipo de espacio deportivo en el metodo ejecutar de la clase ActualizarTipoEspacioDeportivoFachadaImpl. Por favor revise la traza completa del problema";
+
+            throw new BusinessUDElBernabeuException(mensajeUsuario, mensajeTecnico, exception);
+
+
+        } finally {
+            factory.cerrarConexion();
+        }
+
+    }
+
+}
